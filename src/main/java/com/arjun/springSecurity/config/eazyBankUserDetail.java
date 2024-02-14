8652c5fcadde514bca31281061d4c3937b004fa2
Nagarjun <nagarjun7681@gmail.com>
2024-02-14 19:18:24 +0530

    implement new create a Authentication provider and removed the eazyBankuserD etail  file

--- src/main/java/com/arjun/springSecurity/config/eazyBankUserDetail.java
+++ src/main/java/com/arjun/springSecurity/config/eazyBankUserDetail.java
@@ -15,27 +15,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class eazyBankUserDetail implements UserDetailsService {
-
-    @Autowired
-    private CustomerRepository customerRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-       String userName, password;
-        List<GrantedAuthority> authorities ;
-        Optional<Customer> customer = customerRepository.findByEmail(username);
-        if(customer.isEmpty()){
-            throw  new UsernameNotFoundException("User detail not found for the user name "+ username);
-        }
-        else {
-            Customer foundCustomer = customer.get();
-            userName = foundCustomer.getEmail();
-            password = foundCustomer.getPwd();
-            authorities = new ArrayList<>();
-            authorities.add(new SimpleGrantedAuthority(foundCustomer.getRole()));
-        }
-        return new User(userName, password, authorities);
-    }
-}
+//@Service
+//public class eazyBankUserDetail implements UserDetailsService {
+//
+//    @Autowired
+//    private CustomerRepository customerRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//       String userName, password;
+//        List<GrantedAuthority> authorities ;
+//        Optional<Customer> customer = customerRepository.findByEmail(username);
+//        if(customer.isEmpty()){
+//            throw  new UsernameNotFoundException("User detail not found for the user name "+ username);
+//        }
+//        else {
+//            Customer foundCustomer = customer.get();
+//            userName = foundCustomer.getEmail();
+//            password = foundCustomer.getPwd();
+//            authorities = new ArrayList<>();
+//            authorities.add(new SimpleGrantedAuthority(foundCustomer.getRole()));
+//        }
+//        return new User(userName, password, authorities);
+//    }
+//}
